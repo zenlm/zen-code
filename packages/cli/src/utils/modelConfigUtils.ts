0efbf3aba03fe2df34ba7731959709fdddc8a3b2
mingholy.lmh <mingholy.lmh@alibaba-inc.com>
2026-01-19 16:57:01 +0800

    test: add unit tests for modelConfigUtils functions

--- packages/cli/src/utils/modelConfigUtils.ts
+++ packages/cli/src/utils/modelConfigUtils.ts
@@ -44,19 +44,26 @@ export interface ResolvedCliGenerationConfig {
 }
 
 export function getAuthTypeFromEnv(): AuthType | undefined {
-  if (process.env['OPENAI_API_KEY'] && process.env['OPENAI_MODEL']) {
-    return AuthType.USE_OPENAI;
-  }
   if (process.env['QWEN_OAUTH']) {
     return AuthType.QWEN_OAUTH;
+  }
+
+  if (
+    process.env['OPENAI_API_KEY'] &&
+    process.env['OPENAI_MODEL'] &&
+    process.env['OPENAI_BASE_URL']
+  ) {
+    return AuthType.USE_OPENAI;
   }
 
   if (process.env['GEMINI_API_KEY'] && process.env['GEMINI_MODEL']) {
     return AuthType.USE_GEMINI;
   }
+
   if (process.env['GOOGLE_API_KEY'] && process.env['GOOGLE_MODEL']) {
     return AuthType.USE_VERTEX_AI;
   }
+
   if (
     process.env['ANTHROPIC_API_KEY'] &&
     process.env['ANTHROPIC_MODEL'] &&
